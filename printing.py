72b5932a8267edf3f4ddb182120edb0623f1232c
Christopher Helms <cjohnhelms@gmail.com>
2022-09-11 18:59:09 -0500

    Notes on Print Function
    This file goes over the print function with examples, as well as some vocabulary
    like functions, arguments, literals, and return values.

---
+++ printing.py
@@ -0,0 +1,20 @@
+# rint is a funtion. when calling a function, provide arguments inside parenthses
+# hello World in a string literal, so it must be enclosed in single or double quotes
+
+print('Hello World!')
+
+# functions can take more than one argument
+
+print(1 + 2)
+print(7 * 6)
+print()
+print("The End","...or is it?","Now this is the end.")
+
+# a couple more examples. even when not providing an argument, you need parentheses 
+# separate multiple arguments with a comma
+
+# literal - a vlaue of some type, usually numeric or string literals
+# function - a named block of code that we can call by its name. all functions return a value
+# argument - a value passed to a function, in order to give it values to work with
+# calling a function - using the function name to execute the function's code
+# return value - the value that a function returns
