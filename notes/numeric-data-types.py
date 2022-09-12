d80418f0a433aa2b1183b88c1ab662ab9a225d5d
Christopher Helms <cjohnhelms@gmail.com>
2022-09-12 10:27:58 -0500

    2 files - Notes on Variables and Num. Data Types

---
+++ notes/numeric-data-types.py
@@ -0,0 +1,34 @@
+# python built-in data types:
+# numeric
+# iterator
+# sequence
+# mapping
+# file
+# class
+# exception
+
+# this section will look at the numeric and one sequence type
+
+# numeric data types in python 3
+# int
+# float
+# complex
+
+# integers are whole numbers while floats are real numbers
+
+# numeric operators
+
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)   # integer division, rounded down
+print(a % b)    # modulo, remainder after integer devision
+
+# division results in a float, while integer devision will result in an int if an int is needed
+
+# expressions - anything that can be calculated to return a value
+# operators do follow the order of operations, but you can use parenthesis for clarity, even where python does not require them
---
+++ notes/variables.py
@@ -0,0 +1,21 @@
+# everything the program needs ends up stored in memory
+# a variable is another way of giving a meaningful name to an area of memory where we can store values
+
+# python variable names must begin with a letter (upper or lower) or an underscore character
+# they can container letters, numbers, or underscores
+# variables are created when bound to a value with an = 
+
+age = 24
+print(age)
+
+# types:
+# strings
+# integers
+# float (numbers with decimals)
+# because python is dynamically typed, a variable can be giving one type (str) and be changed to an int later on in the same program
+
+print(type(age))
+
+# because python is strongly typed, you cannot contatinate multiple types of data
+
+# print(age + ' years old') - this will not work
