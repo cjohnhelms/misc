4e0043e50e2935f1a676576ab960de4da0c7e88d
Christopher Helms <cjohnhelms@gmail.com>
2022-09-14 11:54:01 -0500

    Improvements to Guessing Game Project
    The guessing game projet is now more efficient and allows for infinite
    guesses. Future steps are to randomly select the answer value and expand
    the range of possibilities, not just 1-10.

--- guessing-game.py
+++ guessing-game.py
@@ -3,19 +3,52 @@ answer = 5
 print('Please guess a number between 1 and 10:')
 guess = int(input())
 
-if guess < answer:
-    print('Too low, enter a second guess:')
-    guess = int(input())
-    if guess == answer:
-        print('Correct')
-    else:
-        print('Sorry, incorrect')
-elif guess > answer:
-    print('Too high, enter a second guess:')
-    guess = int(input())
-    if guess == answer:
-        print('Correct')
-    else:
-        print('Sorry, incorrect')
+# if guess < answer:
+#     print('Too low, enter a second guess:')
+#     guess = int(input())
+#     if guess == answer:
+#         print('Correct')
+#     else:
+#         print('Sorry, incorrect')
+# elif guess > answer:
+#     print('Too high, enter a second guess:')
+#     guess = int(input())
+#     if guess == answer:
+#         print('Correct')
+#     else:
+#         print('Sorry, incorrect')
+# else:
+#     print('You guessed correctly on the first try')
+
+##############################################################################################
+
+# same thing, but more efficient
+
+# if guess != answer:
+#     if guess < answer:
+#         print('Too low, please guess higher')
+#     else:
+#         print('Too high, please guess lower')
+#     guess = int(input())
+#     if guess == answer:
+#         print('Congratulations, that is correct.')
+#     else:
+#         print('Sorry, that is incorrect.')
+# else:
+#     print('Congratulations, you guessed correctly on the first try.')
+
+###############################################################################################
+
+# infinite guesses, instead of just 2
+
+if guess != answer:
+    while guess != answer:
+        if guess < answer:
+            print('Too low, please guess higher')
+        else:
+            print('Too high, please guess lower')
+        guess = int(input())
+        if guess == answer:
+            print('Congratulations, you are correct.')
 else:
-    print('You guessed correctly on the first try')
+    print('Congratulations, you guessed correctly on the first try.')
