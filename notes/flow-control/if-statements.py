b47d12d6264ba19a510749eccfcf9ff9bfe6a37e
Christopher Helms <cjohnhelms@gmail.com>
2022-09-14 10:12:05 -0500

    Adding Elif Statements to If Statement Notes

--- notes/flow-control/if-statements.py
+++ notes/flow-control/if-statements.py
@@ -2,10 +2,22 @@ name = input('Please enter your name: ')
 age = int(input('How old are you {0}? '.format(name)))
 print(age)
 
-if age >= 18:
-    print("You are old enough to vote.")
-    print('Please put an X in the box.')
-else:
-    print('Please come back in {0} years.'.format(18 - age))
+# if age >= 18:
+#     print("You are old enough to vote.")
+#     print('Please put an X in the box.')
+# else:
+#     print('Please come back in {0} years.'.format(18 - age))
 
 # statements in python are denoted by a : which starts a new code block
+
+print()
+
+# elif example
+
+if age < 18:
+    print('Please come back in {0} years.'.format(18 - age))
+elif age == 900:
+    print('You should be dead by now.')
+else:
+    print("You are old enough to vote.")
+    print('Please put an X in the box.')
