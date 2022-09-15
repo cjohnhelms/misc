9acd24eaf82c928fffb537971353cf4f5a0765e5
Christopher Helms <cjohnhelms@gmail.com>
2022-09-15 09:37:38 -0500

    Adding Notes on Conditional Statements
    Includes notes on the differences between and and or statements.

---
+++ notes/flow-control/conditions.py
@@ -0,0 +1,7 @@
+age = int(input('How old are you? '))
+
+if age >= 16 and age <16:
+    print('You can drive, but no social security for you.')
+
+# and requires an agreement to both parts on the condition
+# or only requires one condition to be true, both both conditions being true would also satisfy this
