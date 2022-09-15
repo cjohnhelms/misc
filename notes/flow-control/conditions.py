cae9f96da9d5325f25be2e84aa2836027b7b63da
Christopher Helms <cjohnhelms@gmail.com>
2022-09-15 09:46:28 -0500

    Conditionals Notes Edit
    Added information on simplifying chained conditionals.

--- notes/flow-control/conditions.py
+++ notes/flow-control/conditions.py
@@ -1,7 +1,16 @@
 age = int(input('How old are you? '))
 
-if age >= 16 and age <16:
-    print('You can drive, but no social security for you.')
+# if age >= 16 and age <16:
+#     print('You can drive, but no social security for you.')
 
 # and requires an agreement to both parts on the condition
 # or only requires one condition to be true, both both conditions being true would also satisfy this
+
+# simplifying chained comparisons
+
+if 16 <= age < 65:
+    print('You can drive, but no social security for you.')
+elif age >= 65:
+    print('Congrats on your retirement.')
+else:
+    print('You\'re too young to drive.')
