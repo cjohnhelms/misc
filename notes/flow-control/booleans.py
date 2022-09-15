bd8c779e203a125d317be4d09b37f2bdfa12f9fd
Christopher Helms <cjohnhelms@gmail.com>
2022-09-15 10:23:27 -0500

    Boolean Notes

---
+++ notes/flow-control/booleans.py
@@ -0,0 +1,13 @@
+# a boolean value is a value that can either be True or False 
+# there are only two values that a boolean can have
+# a boolean expression is an expression that evaluates to either true or false
+# booleans aren't restricted to testing numbers, they can also test string values
+
+day = 'Monday'
+temperature = 30
+raining = True
+
+if day ==  'Saturday' and temperature > 27 and not raining:
+    print('Go swimming')
+else:
+    print('Learn Python')
