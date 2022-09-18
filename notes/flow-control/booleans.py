c1d8273e3e033cb821000fbc91b064e323d88d19
Christopher Helms <cjohnhelms@gmail.com>
2022-09-18 15:11:07 -0500

    Edits to Conditionals and Booleans

--- notes/flow-control/booleans.py
+++ notes/flow-control/booleans.py
@@ -7,7 +7,7 @@ day = 'Monday'
 temperature = 30
 raining = True
 
-if day ==  'Saturday' and temperature > 27 and not raining:
+if (day ==  'Saturday' and temperature > 27) and not raining:
     print('Go swimming')
 else:
     print('Learn Python')
---
+++ notes/flow-control/conditional-operators.py
@@ -0,0 +1,7 @@
+# condiditonal operators
+# less than <
+# greater than >
+# less than or equal to <=
+# greater than or equal to >=
+# equal to ==
+# not equal to !=
