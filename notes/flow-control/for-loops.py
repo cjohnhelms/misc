add77bc0daec874419a81bd855a4f5a5b53b6157
Christopher Helms <cjohnhelms@gmail.com>
2022-09-18 11:00:00 -0500

    Notes on For Loops

---
+++ notes/flow-control/for-loops.py
@@ -0,0 +1,9 @@
+# for loops work by iterating over some set of values
+# it assigns each of the values, one by one, to one or more variables
+# executes a block of code once for each value
+# an iterable object is anything that can be iterated over, like a sequence
+
+parrot = 'Norwegian Blue'
+
+for character in parrot:
+    print(character)
