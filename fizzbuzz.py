32003b1c44424404ae0b6bb60e35f325d045c551
Christopher Helms <cjohnhelms@gmail.com>
2022-09-22 12:04:58 -0500

    Added Problem Explanation Comment to FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,5 @@
+# program that prints out the integers from 1 to 100, except we're going to replace integers with words when they're multiples of three multiples or five, or if they're multiples of both three and five.
+
 for num in range(1,101):
     if (num % 3) == 0 and (num % 5) == 0:
         print('fizzbuzz')
