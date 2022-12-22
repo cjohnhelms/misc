3ec46cf87a54a881c3d12a8d2f20534c29b32980
Chris Helms <cjohnhelms@gmail.com>
2022-12-22 17:19:02 -0600

    Changed tabs to spaces
    Vim config wasn't functioning properly, needed to change tabs to correct 4 space indentions.

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -5,15 +5,15 @@ import pygame
 class Ship:
     def __init__(self, ai_game):
     # Initialize ship and set stating position
-	self.screen = ai_game.screen
-	self.screen_rect = ai_game.screen.get_rect()
+    self.screen = ai_game.screen
+    self.screen_rect = ai_game.screen.get_rect()
 
-	# Load the ship image and get its rect
-	self.image = pygame.image.load('images/ship.bmp')
-	self.rect = self.image.get_rect()
+    # Load the ship image and get its rect
+    self.image = pygame.image.load('images/ship.bmp')
+    self.rect = self.image.get_rect()
 
-	# Start each new ship at the bottom center of the screen 
-	self.rect.midbottom = self.screen_rect.midbottom
+    # Start each new ship at the bottom center of the screen 
+    self.rect.midbottom = self.screen_rect.midbottom
 
 def blitme(self):
     self.screen.blit(self.image, self.rect)
